4199b5be8c6a497b225ce2065f1c040d44c86ee6
Nataliia <Nata13pr@gmail.com>
2023-08-10 00:20:00 +0300

    added StarRating

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import StarRating from './StarRating';
+// import './index.css';
+// import App from './App';
 
+function Test(){
+const [movieRating,setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating}/>
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} messages={['Terrible','Bad','Okay','Good','Amazing']}/>
+    <StarRating size={24} color='red' className='test'/>
+    <StarRating size={48} color='blue' defaultRating={3}/>
+    <StarRating size={24} color='violet'/>
+    <Test/>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
